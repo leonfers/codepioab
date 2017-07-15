9ed75014bf10759339f658e62d42e51bbeec462a
Leoncio Ferreira <leonfers@hotmail.com>
2017-07-15 02:19:26 -0300

    Intent para registro

--- app/src/main/java/com/example/leonf/advogram/StartActivity.java
+++ app/src/main/java/com/example/leonf/advogram/StartActivity.java
@@ -1,13 +1,30 @@
 package com.example.leonf.advogram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    private Button regbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        regbutton =(Button)findViewById(R.id.botao_registro);
+
+        regbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg_intent = new Intent(StartActivity.this ,RegisterActivity.class);
+
+                startActivity(reg_intent);
+            }
+        });
     }
+
 }
