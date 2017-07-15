5539a45301c7c1a451075a2a12325827b83d5ce6
Leoncio Ferreira <leonfers@hotmail.com>
2017-07-15 09:32:25 -0300

    Subimissao de novo usuario para o Firebase

--- app/src/main/java/com/example/leonf/advogram/RegisterActivity.java
+++ app/src/main/java/com/example/leonf/advogram/RegisterActivity.java
@@ -1,10 +1,10 @@
 package com.example.leonf.advogram;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +19,7 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout mDisplayName;
     private TextInputLayout mEmail;
     private TextInputLayout mPassword;
-    private Button mRegisterButton;
+
     private FirebaseAuth mAuth;
 
     //Firebase
@@ -35,36 +35,48 @@ public class RegisterActivity extends AppCompatActivity {
         mDisplayName = (TextInputLayout) findViewById(R.id.Name_input);
         mEmail = (TextInputLayout) findViewById(R.id.Email_input);
         mPassword = (TextInputLayout) findViewById(R.id.Password_input);
-        mRegisterButton = (Button) findViewById(R.id.botao_para_registrar);
+        Button mRegisterButton = (Button) findViewById(R.id.botao_para_registrar);
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 String display_name = mDisplayName.getEditText().getText().toString();
+                Log.d("Nome",display_name);
                 String email = mEmail.getEditText().getText().toString();
+                Log.d("email",email);
                 String password = mPassword.getEditText().getText().toString();
+                Log.d("senha",password);
 
-                register_user(display_name,email,password);
+                register_user(email,password);
             }
         });
 
 
     }
 
-    private void register_user(String display_name, String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
-                    startActivity(mainIntent);
-                    finish();
-                }else{
-                    Toast.makeText(RegisterActivity.this, "Ocorreu um erro", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+    private void register_user( String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Ocorreu um erro", Toast.LENGTH_LONG).show();
+                        }
 
 
+
+                    }
+
+                });
     }
 }
+
+
